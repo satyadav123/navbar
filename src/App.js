f88445e519582234e1d34e145ac1883c76bc77de
satyadav123 <sk259454@gmail.com>
2022-04-21 08:30:46 +0530

    Navbar-react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+const link=["Services","Project","About"];
+const logo="LOGOBAKERY";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="nav">
+<div><h3>{logo}</h3></div>
+<div className="link"> {link.map((e)=>{
+         return <div><p>{e}</p></div>
+         })}</div>
+<div className="button"><button>connect</button></div>
+
+
+    </div>
     </div>
   );
 }
-
-export default App;
